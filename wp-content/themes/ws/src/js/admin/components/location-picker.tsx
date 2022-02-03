897baf1b6e49a8ee6b0541a2e924f742af5e1f10
RiceCrisp <blakeboucher@gmail.com>
2022-02-03 14:21:31 -0600

    Update location-picker.tsx

--- wp-content/themes/ws/src/js/admin/components/location-picker.tsx
+++ wp-content/themes/ws/src/js/admin/components/location-picker.tsx
@@ -57,13 +57,13 @@ export function LocationPicker(props: ComponentProps) {
     if (scriptElement) {
       scriptElement.addEventListener('load', mapLoad)
     }
-    else {
+    else if (mapKey) {
       const googleMapScript = document.createElement('script')
       googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${mapKey}`
       window.document.body.appendChild(googleMapScript)
       googleMapScript.addEventListener('load', mapLoad)
     }
-  }, [])
+  }, [mapKey])
 
   useEffect(() => {
     if (map && geocoder && marker) {
